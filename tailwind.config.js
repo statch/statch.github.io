1e4da4d49353beede0984389e9652a16ad5afd11
Kevin Thomas <46242684+TrustedMercury@users.noreply.github.com>
2020-10-30 20:06:19 -0400

    Added source code

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        xblue: "#0E67B4",
+        dblue: "#0C5A9E",
+        xgold: "#F1984B"
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
